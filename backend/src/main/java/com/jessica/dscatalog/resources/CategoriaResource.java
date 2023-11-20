90109745baff334b3214b8e53716da4dacc328fc
jessicaklier <jhessicadesouza12@hotmail.com>
2023-11-20 13:55:26 -0300

    Encontrar categoria por id, tratamento de exceções

--- backend/src/main/java/com/jessica/dscatalog/resources/CategoriaResource.java
+++ backend/src/main/java/com/jessica/dscatalog/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll(){ // findAll(Busca todas as categorias)
 		List<CategoriaDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id){ // findAll(Busca todas as categorias)
+		CategoriaDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/jessica/dscatalog/services/CategoriaService.java
+++ backend/src/main/java/com/jessica/dscatalog/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.jessica.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jessica.dscatalog.dto.CategoriaDTO;
 import com.jessica.dscatalog.entities.Categoria;
 import com.jessica.dscatalog.repositories.CategoriaRepository;
+import com.jessica.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -23,5 +25,12 @@ public class CategoriaService {
 
 		return list.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
 		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoriaDTO findById(Long id) {
+		Optional<Categoria> obj = repository.findById(id);
+		Categoria entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		return new CategoriaDTO(entity);
 	}
 }
